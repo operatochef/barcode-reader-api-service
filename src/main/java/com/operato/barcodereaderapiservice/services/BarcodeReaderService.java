990fb8f21e1125dfa2fecdeb08ed37f766244c37
Jinwon <41100208+vincent841@users.noreply.github.com>
2021-12-01 17:38:54 +0900

    Update BarcodeReaderService.java

--- src/main/java/com/operato/barcodereaderapiservice/services/BarcodeReaderService.java
+++ src/main/java/com/operato/barcodereaderapiservice/services/BarcodeReaderService.java
@@ -23,12 +23,23 @@ public class BarcodeReaderService {
   }
 
   public JSONObject getData(List<BarcodeReaderRequestBody> requestBodies) {
+
+    System.out.println("Request Body: ");
+    for (BarcodeReaderRequestBody requestBody : requestBodies) {
+      System.out.println("Name: " + requestBody.getName());
+      System.out.println("Format: " + requestBody.getFormat());
+      System.out.println("Data: " + requestBody.getData().length());
+      System.out.println("");
+    }
+
     try {
       BarcodeReaderResponseBody responseBody = new BarcodeReaderResponseBody();
       for (BarcodeReaderRequestBody requestBody : requestBodies) {
         String imageData = requestBody.getData();
 
         BarcodeReaderData barcodeData = BarcodeReader.decodeBase64(imageData);
+        System.out.println("Decode result: " + barcodeData.getStatusCode());
+
         responseBody.addResult(barcodeData);
         if (barcodeData.getStatusCode() == "200") {
           responseBody.incSuccessCount();
